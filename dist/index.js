42e35802aa7952cab4259a410f6dd31844e7aca2
Edward Byne <ejbyne@gmail.com>
2017-02-07 22:11:41 +0100

    Git ignoring dist folder

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var path = require("path");
-var getTestOutput_1 = require("./getTestOutput");
-var fileName = path.join(__dirname, '../data/graph.txt');
-getTestOutput_1.default(fileName)
-    .then(function (testOutput) { return console.log(testOutput); });
--- dist/test/utils/testSetUp.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var unexpected_1 = require("unexpected");
-var expect = unexpected_1.default.clone();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = expect;
