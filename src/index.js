35740c308ad94b9824e8084a2f93d53bd893e5a6
Edward Byne <ejbyne@gmail.com>
2017-02-07 22:17:23 +0100

    Removing js files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import * as path from 'path';
-import getTestOutput from './getTestOutput';
-const fileName = path.join(__dirname, '../data/graph.txt');
-getTestOutput(fileName)
-    .then(testOutput => console.log(testOutput));
